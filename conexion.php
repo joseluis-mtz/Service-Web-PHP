b67564eef2cb95518a2861023742a06b29b8355c
JoseLuis <joseluismartinez1024@gmail.com>
2022-01-06 17:37:10 -0600

    Crear archivo de conexión a MySQL.

---
+++ conexion.php
@@ -0,0 +1,26 @@
+<?php
+
+class Conexion
+{
+	private $driver = "mysql";
+	private $host = "localhost";
+	private $user = "joseluis";
+	private $password = "adminroot";
+	private $database = "estLuna";
+	private $charset = "utf8";
+
+	function __construct()
+	{
+	}
+
+	protected function conectar()
+	{
+		try {
+			$objConexion = new PDO("{$this->driver}: host={$this->host}; dbname={$this->database}; charset={$this->charset}", $this->user, $this->password);
+			$objConexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $objConexion;
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
+}
